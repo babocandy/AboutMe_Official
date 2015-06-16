02bbb5e79d65c164910b040a376e5712c93c64fc
hsungwoong <sw216@naver.com>
2015-06-16 10:15:28 +0900

    HSW 20150616 2

---
+++ AboutMe.Domain/Entity/AdminProduct/AdminProduct_EntityPartial.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace AboutMe.Domain.Entity.AdminProduct
+{
+    
+    /*
+    class AdminProduct_EntityPartial
+    {
+    } */
+
+    [MetadataType(typeof(AdminProduct_Metadata))]
+    public partial class TB_PRODUCT_INFO
+    {
+        public string SEARCH_KEY { get; set; }
+        public string SEARCH_KEYWORD { get; set; }
+
+    }
+}
---
+++ AboutMe.Domain/Entity/AdminProduct/AdminProduct_Metadata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AboutMe.Domain.Entity.AdminProduct
+{
+    class AdminProduct_Metadata
+    {
+
+        [Required(ErrorMessage = "키를 입력해주세요")]
+        public string P_CATE_CODE { get; set; }
+    }
+}
