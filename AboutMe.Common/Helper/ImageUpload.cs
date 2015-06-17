455bd890577859225f3d988f03a01862f7f88764
hsungwoong <sw216@naver.com>
2015-06-17 16:29:49 +0900

    commit 20150617 hsw1

---
+++ AboutMe.Common/Helper/ImageUpload.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Web;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Drawing.Drawing2D;
+using AboutMe.Common.Data;
+
+namespace AboutMe.Common.Helper
+{
+    public class ImageUpload
+    {
+        // set default size here
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        // folder for the upload, you can put this in the web.config
+        public string UploadPath = "~/Upload/"; //이미지 업로드 경로 default /Upload/
+        public bool addMobileImage = false; //이미지 업로드시 모바일 업로드용으로 리사이즈추가 필요시 true
+
+        #region 고유한 이미지네임으로 변경
+        public ImageResult RenameUploadFile(HttpPostedFileBase file, Int32 counter = 0)
+        {
+            var fileName = Path.GetFileName(file.FileName);
+
+            string prepend = "item_";
+            string finalFileName = prepend + ((counter).ToString()) + "_" + fileName;
+            if (System.IO.File.Exists
+                (HttpContext.Current.Request.MapPath(UploadPath + finalFileName)))
+            {
+                //file exists => add country try again
+                return RenameUploadFile(file, ++counter);
+            }
+            //file doesn't exist, upload item but validate first
+            return UploadFile(file, finalFileName);
+            
+        }
+        #endregion
+
+        #region 이미지 업로드
+        private ImageResult UploadFile(HttpPostedFileBase file, string fileName)
+        {
+            ImageResult imageResult = new ImageResult { Success = true, ErrorMessage = null };
+
+            var path = Path.Combine(HttpContext.Current.Request.MapPath(UploadPath), fileName);
+
+            //모바일 path
+            var mobile_path = Path.Combine(HttpContext.Current.Request.MapPath(UploadPath), "mobile_"+fileName);
+
+            string extension = Path.GetExtension(file.FileName);
+
+            //make sure the file is valid
+            if (!ValidateExtension(extension))
+            {
+                imageResult.Success = false;
+                imageResult.ErrorMessage = "Invalid Extension";
+                return imageResult;
+            }
+
+            try
+            {
+                file.SaveAs(path);
+                
+                Image imgOriginal = Image.FromFile(path);
+                //pass in whatever value you want
+                Image imgActual = Scale(imgOriginal);
+                imgOriginal.Dispose();
+                imgActual.Save(path);
+                imgActual.Dispose();
+
+                imageResult.ImageName = fileName;
+
+                if (addMobileImage) //모바일 이미지 추가시
+                {
+                    file.SaveAs(mobile_path);
+                    Image mobile_imgOriginal = Image.FromFile(mobile_path);
+                    this.Width = 300; //모바일 사이즈
+
+                    //pass in whatever value you want
+                    Image mobile_imgActual = Scale(mobile_imgOriginal);
+                    mobile_imgOriginal.Dispose();
+                    mobile_imgActual.Save(mobile_path);
+                    mobile_imgActual.Dispose();
+
+                }
+
+                return imageResult;
+            }
+            catch (Exception ex)
+            {
+                // you might NOT want to show the exception error for the user
+                // this is generally logging or testing
+
+                imageResult.Success = false;
+                imageResult.ErrorMessage = ex.Message;
+                return imageResult;
+            }
+        }
+        #endregion
+
+        #region 파일이 이미지 타입인지 체크
+        private bool ValidateExtension(string extension)
+        {
+            extension = extension.ToLower();
+            switch (extension)
+            {
+                case ".jpg":
+                    return true;
+                case ".png":
+                    return true;
+                case ".gif":
+                    return true;
+                case ".jpeg":
+                    return true;
+                default:
+                    return false;
+            }
+        }
+        #endregion
+
+        #region 이미지 resize
+        private Image Scale(Image imgPhoto)
+        {
+            float sourceWidth = imgPhoto.Width;
+            float sourceHeight = imgPhoto.Height;
+            float destHeight = 0;
+            float destWidth = 0;
+            int sourceX = 0;
+            int sourceY = 0;
+            int destX = 0;
+            int destY = 0;
+
+            // force resize, might distort image
+            if (Width != 0 && Height != 0)
+            {
+                destWidth = Width;
+                destHeight = Height;
+            }
+            // change size proportially depending on width or height
+            else if (Height != 0)
+            {
+                destWidth = (float)(Height * sourceWidth) / sourceHeight;
+                destHeight = Height;
+            }
+            else
+            {
+                destWidth = Width;
+                destHeight = (float)(sourceHeight * Width / sourceWidth);
+            }
+
+            Bitmap bmPhoto = new Bitmap((int)destWidth, (int)destHeight,
+                                        PixelFormat.Format32bppPArgb);
+            bmPhoto.SetResolution(imgPhoto.HorizontalResolution, imgPhoto.VerticalResolution);
+
+            Graphics grPhoto = Graphics.FromImage(bmPhoto);
+            grPhoto.InterpolationMode = InterpolationMode.HighQualityBicubic;
+
+            grPhoto.DrawImage(imgPhoto,
+                new Rectangle(destX, destY, (int)destWidth, (int)destHeight),
+                new Rectangle(sourceX, sourceY, (int)sourceWidth, (int)sourceHeight),
+                GraphicsUnit.Pixel);
+
+            grPhoto.Dispose();
+
+            return bmPhoto;
+        }
+        #endregion
+
+    }
+}
--- AboutMe.Web.Admin/Controllers/AdminProductController.cs
+++ AboutMe.Web.Admin/Controllers/AdminProductController.cs
@@ -8,6 +8,7 @@ using AboutMe.Common.Helper;
 using AboutMe.Domain.Service.AdminProduct;
 using AboutMe.Domain.Entity.AdminProduct;
 using System.Data.Entity.Core.Objects;
+using AboutMe.Common.Data;
 
 namespace AboutMe.Web.Admin.Controllers
 {
@@ -118,16 +119,111 @@ namespace AboutMe.Web.Admin.Controllers
         [ValidateAntiForgeryToken]
        
         //public ActionResult ProductInsert(string P_CATE_CODE, string C_CATE_CODE, string L_CATE_CODE, string P_CODE, string P_NAME, Nullable<int> P_COUNT, Nullable<int> P_POINT, Nullable<int> P_PRICE, Nullable<int> SELLING_PRICE, Nullable<int> DISCOUNT_RATE, Nullable<int> DISCOUNT_P_POINT, Nullable<int> DISCOUNT_PRICE, string SOLDOUT_YN, string P_INFO_DETAIL_WEB, string P_INFO_DETAIL_MOBILE, string MV_URL, string P_COMPONENT_INFO, string P_TAG, string MAIN_IMG, string OTHER_IMG1, string OTHER_IMG2, string OTHER_IMG3, string OTHER_IMG4, string OTHER_IMG5, string DISPLAY_YN, string ICON_YN, string WITH_PRODUCT_LIST)
-        public ActionResult ProductInsert(TB_PRODUCT_INFO tb_product_info)
+        public ActionResult ProductInsert(TB_PRODUCT_INFO tb_product_info, HttpPostedFileBase MAIN_IMG, HttpPostedFileBase OTHER_IMG1, HttpPostedFileBase OTHER_IMG2, HttpPostedFileBase OTHER_IMG3, HttpPostedFileBase OTHER_IMG4, HttpPostedFileBase OTHER_IMG5)
         {
-
-            try
+            if (ModelState.IsValid)
             {
+                string Product_path = AboutMe.Common.Helper.Config.GetConfigValue("ProductPhotoPath");
+
+                #region 파일 업로드
+                if (MAIN_IMG != null)
+                {
+                    //MAIN_IMG.SaveAs(Server.MapPath(Product_path) + MAIN_IMG.FileName);
+                    ImageUpload imageUpload = new ImageUpload { Width = 600, UploadPath = Product_path, addMobileImage = true };
+
+                    // rename, resize, and upload
+                    //return object that contains {bool Success,string ErrorMessage,string ImageName}
+                    ImageResult imageResult = imageUpload.RenameUploadFile(MAIN_IMG);
+                    if (imageResult.Success)
+                    {
+                        tb_product_info.MAIN_IMG = imageResult.ImageName;
+                    }
+                    else
+                    {
+                        //ViewBag.Error = imageResult.ErrorMessage;
+                        tb_product_info.MAIN_IMG = "";
+                    }
+                }
+
+                if (OTHER_IMG1 != null)
+                {
+                    //OTHER_IMG1.SaveAs(Server.MapPath(Product_path) + OTHER_IMG1.FileName);
+                    //tb_product_info.OTHER_IMG1 = OTHER_IMG1.FileName;
+
+                    ImageUpload imageUpload1 = new ImageUpload { Width = 600, UploadPath = Product_path, addMobileImage = true };
+                    ImageResult imageResult1 = imageUpload1.RenameUploadFile(OTHER_IMG1);
+                    if (imageResult1.Success)
+                    {
+                        tb_product_info.OTHER_IMG1 = imageResult1.ImageName;
+                    }
+                    else
+                    {
+                        tb_product_info.OTHER_IMG1 = "";
+                    }
+
+                }
+                if (OTHER_IMG2 != null)
+                {
+                    //OTHER_IMG2.SaveAs(Server.MapPath(Product_path) + OTHER_IMG2.FileName);
+                    //tb_product_info.OTHER_IMG2 = OTHER_IMG2.FileName;
+
+                    ImageUpload imageUpload2 = new ImageUpload { Width = 600, UploadPath = Product_path, addMobileImage = true };
+                    ImageResult imageResult2 = imageUpload2.RenameUploadFile(OTHER_IMG2);
+                    if (imageResult2.Success)
+                    {
+                        tb_product_info.OTHER_IMG2 = imageResult2.ImageName;
+                    }
+                    else
+                    {
+                        tb_product_info.OTHER_IMG2 = "";
+                    }
+                }
+                if (OTHER_IMG3 != null)
+                {
+                    ImageUpload imageUpload3 = new ImageUpload { Width = 600, UploadPath = Product_path, addMobileImage = true };
+                    ImageResult imageResult3 = imageUpload3.RenameUploadFile(OTHER_IMG3);
+                    if (imageResult3.Success)
+                    {
+                        tb_product_info.OTHER_IMG3 = imageResult3.ImageName;
+                    }
+                    else
+                    {
+                        tb_product_info.OTHER_IMG3 = "";
+                    }
+                }
+                if (OTHER_IMG4 != null)
+                {
+                    ImageUpload imageUpload4 = new ImageUpload { Width = 600, UploadPath = Product_path, addMobileImage = true };
+                    ImageResult imageResult4 = imageUpload4.RenameUploadFile(OTHER_IMG4);
+                    if (imageResult4.Success)
+                    {
+                        tb_product_info.OTHER_IMG4 = imageResult4.ImageName;
+                    }
+                    else
+                    {
+                        tb_product_info.OTHER_IMG4 = "";
+                    }
+                }
+                if (OTHER_IMG5 != null)
+                {
+                    ImageUpload imageUpload5 = new ImageUpload { Width = 600, UploadPath = Product_path, addMobileImage = true };
+                    ImageResult imageResult5 = imageUpload5.RenameUploadFile(OTHER_IMG5);
+                    if (imageResult5.Success)
+                    {
+                        tb_product_info.OTHER_IMG5 = imageResult5.ImageName;
+                    }
+                    else
+                    {
+                        tb_product_info.OTHER_IMG5 = "";
+                    }
+                }
+                #endregion
+
                 //_AdminProductService.InsertAdminProduct(P_CATE_CODE, C_CATE_CODE, L_CATE_CODE, P_CODE, P_NAME, P_COUNT, P_POINT, P_PRICE, SELLING_PRICE, DISCOUNT_RATE, DISCOUNT_P_POINT, DISCOUNT_PRICE, SOLDOUT_YN, P_INFO_DETAIL_WEB, P_INFO_DETAIL_MOBILE, MV_URL, P_COMPONENT_INFO, P_TAG, MAIN_IMG, OTHER_IMG1, OTHER_IMG2, OTHER_IMG3, OTHER_IMG4, OTHER_IMG5, DISPLAY_YN, ICON_YN, WITH_PRODUCT_LIST);
                 _AdminProductService.InsertAdminProduct(tb_product_info);
                 return RedirectToAction("ProductIndex", new { SearchCol = "" });
             }
-            catch
+            else
             {
                 return View();
             }
