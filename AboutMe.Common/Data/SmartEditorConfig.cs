74e45d7a880c02685959afea600a2d7f90630081
dykimcstone <dykim@cstone.co.kr>
2015-07-01 18:35:34 +0900

    smart editor 삭제

---
+++ AboutMe.Common/Data/SmartEditorConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AboutMe.Common.Data
+{
+    public class SmartEditorConfig
+    {
+        public int width { set; get; }
+        public int height { set; get; }
+        public string tfID { set; get; }
+        public string memo { set; get; }
+    }
+}
