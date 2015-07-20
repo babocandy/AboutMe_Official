3f339e23926812ba3e5ec2605607e479501579de
dykimcstone <dykim@cstone.co.kr>
2015-07-20 22:05:11 +0900

    my review

--- AboutMe.Domain/Service/Review/IReviewService.cs
+++ AboutMe.Domain/Service/Review/IReviewService.cs
@@ -7,10 +7,12 @@ using System.Threading.Tasks;
 using AboutMe.Domain.Entity.Product;
 using System.Data.Entity.Core.Objects;
 
+using AboutMe.Domain.Entity.Review;
+
 namespace AboutMe.Domain.Service.Review
 {
     public interface IReviewService
     {
-       //
+        List<SP_REVIEW_PRODUCT_READY_SEL_Result> GetReadyList(string mid);
     }
 }
