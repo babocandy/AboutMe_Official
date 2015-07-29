92d59bcd77f1aca52e9a9fc7dcf0c734065be8c1
dykimcstone <dykim@cstone.co.kr>
2015-07-29 15:48:39 +0900

    add display admin

---
+++ AboutMe.Web.Admin/Controllers/AdminDisplayController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using System.Diagnostics;
+using AboutMe.Web.Admin.Models;
+using AboutMe.Web.Admin.Common.Filters;
+using AboutMe.Web.Admin.Common;
+
+namespace AboutMe.Web.Admin.Controllers
+{
+    public class AdminDisplayController : Controller
+    {
+        [CustomAuthorize]
+        public ActionResult Index()
+        {
+            AdminDisplayWebMainViewModel model = new AdminDisplayWebMainViewModel();
+            return View(model);
+        }
+
+        public ActionResult SaveWebMainBanner()
+        {
+            return View();
+        }
+
+        public ActionResult RemoveWebMainBanner()
+        {
+            return View();
+        }
+
+        public ActionResult MobileMain()
+        {
+            return View();
+        }
+
+        public ActionResult CartBanner()
+        {
+            return View();
+        }
+
+        public ActionResult GBNBanner()
+        {
+            return View();
+        }
+
+        public ActionResult ProductDetailBanner()
+        {
+            return View();
+        }
+
+        public ActionResult Popup()
+        {
+            return View();
+        }
+    }
+}
