fc10d023e2af8b7d82b2fc64accada50312f2d8e
bright8888 <bright8888@nate.com>
2015-07-29 21:15:03 +0900

    jjjj

--- AboutMe.Web.Admin/App_Start/RouteConfig.cs
+++ AboutMe.Web.Admin/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace AboutMe.Web.Admin
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "AdminUser", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "AdminFrontMember", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
