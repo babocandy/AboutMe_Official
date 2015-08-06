aa01021013d5f2e6f6bb191b2dcd74f209ad8c58
bckang <bckang@gxensoft.com>
2015-08-06 17:51:06 +0900

    event management

---
+++ AboutMe.Domain/Entity/Event/EventUserModel.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Web;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AboutMe.Domain.Entity.Common;
+
+namespace AboutMe.Domain.Entity.Event
+{
+    public class EVENT_ADMIN_SEARCH
+    {
+        private string fromdate;
+        private string todate;
+        private string inggbn;  // 진행구분  0:전체, 1:예약, 2:진행, 3:종료
+        private string useyn;     // 전시구분 A:전체, Y:전시, N:비전시
+        private string searchcol;
+        private string searchkeyword;
+        private int? page;
+        private int? pagesize;
+
+
+        public string FromDate
+        {
+            get
+            {
+                return this.fromdate ?? "";
+            }
+            set
+            {
+                this.fromdate = value;
+            }
+        }
+
+        public string ToDate
+        {
+            get
+            {
+                return this.todate ?? "";
+            }
+            set
+            {
+                this.todate = value;
+            }
+        }
+
+        public string IngGbn
+        {
+            get
+            {
+                return this.inggbn ?? "0";
+            }
+            set
+            {
+                this.inggbn = value;
+            }
+        }
+
+
+        public string UseYn
+        {
+            get
+            {
+                return this.useyn ?? "A";
+            }
+            set
+            {
+                this.useyn = value;
+            }
+        }
+
+        public string SearchCol
+        {
+            get
+            {
+                return this.searchcol ?? "";
+            }
+            set
+            {
+                this.searchcol = value;
+            }
+        }
+
+
+        public string SearchKeyword
+        {
+            get
+            {
+                return this.searchkeyword ?? "";
+            }
+            set
+            {
+                this.searchkeyword = value;
+            }
+        }
+
+
+        public int? Page
+        {
+            get
+            {
+                return this.page ?? 1;
+            }
+            set
+            {
+                this.page = value;
+            }
+        }
+
+        public int? PageSize
+        {
+            get
+            {
+                return this.pagesize ?? 10;
+            }
+            set
+            {
+                this.pagesize = value;
+            }
+        }
+    }
+
+    public class EVENT_ADMIN_EDIT
+    {
+        public int IDX { get; set; }
+        public string Mode { get; set; }
+        public EVENT_ADMIN_SEARCH SearchOption { get; set; }
+        public SP_ADMIN_EVENT_VIEW_Result EventInfo { get; set; }
+    }
+    
+    public class EVENT_ADMIN_INDEX
+    {
+        public int ListCnt { get; set; }
+        public EVENT_ADMIN_SEARCH SearchOption { get; set; }
+        public List<SP_ADMIN_EVENT_SEL_Result> ListInfo { get; set; }
+    }
+
+    public class EVENT_SAVE_PARAM
+    {
+
+        public int IDX { get; set; }
+        public string EVENT_NAME { get; set; }
+        public string FROM_DATE { get; set; }
+        public string FROM_TIME { get; set; }
+        public string TO_DATE { get; set; }
+        public string TO_TIME { get; set; }
+        public string EVENT_GBN { get; set; }
+        public string WEB_CONTENTS { get; set; }
+        public HttpPostedFileBase MOBILE_FILE { get; set; }
+        public HttpPostedFileBase WEB_BANNER { get; set; }
+        public HttpPostedFileBase MOBILE_BANNER { get; set; }
+        public string MOBILE_FILE_DEL { get; set; }
+        public string WEB_BANNER_DEL { get; set; }
+        public string MOBILE_BANNER_DEL { get; set; }
+        public string OLD_MOBILE_FILE { get; set; }
+        public string OLD_WEB_BANNER { get; set; }
+        public string OLD_MOBILE_BANNER { get; set; }
+        public string WEB_URL { get; set; }
+        public string MOBILE_URL { get; set; }
+        public string USE_YN { get; set; }
+        public string ADM_ID { get; set; }
+
+    }
+}
---
+++ AboutMe.Web.Admin/Controllers/EventController.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+using AboutMe.Common.Helper;
+using AboutMe.Domain.Service.Event;
+using AboutMe.Domain.Entity.Event;
+
+using AboutMe.Web.Admin.Common.Filters;
+using AboutMe.Web.Admin.Common;
+using AboutMe.Common.Data;
+
+namespace AboutMe.Web.Admin.Controllers
+{
+    [CustomAuthorize]
+    public class EventController : BaseAdminController
+    {
+        private IEventService _eventservice;
+        private string _eventUploadPath = AboutMe.Common.Helper.Config.GetConfigValue("EventUploadPath");
+
+        public EventController(IEventService _eventservice)
+        {
+            this._eventservice = _eventservice;
+        }
+
+        public ActionResult Main()
+        {
+            return View();
+        }
+
+        public ActionResult Index(EVENT_ADMIN_SEARCH SearchParam)
+        {
+            EVENT_ADMIN_INDEX M = new EVENT_ADMIN_INDEX
+            {
+                ListCnt = _eventservice.EventAdminCount(SearchParam)
+                ,
+                SearchOption = SearchParam
+                ,
+                ListInfo = _eventservice.EventAdminList(SearchParam)
+
+            };
+            return View(M);
+        }
+
+        public ActionResult New(EVENT_ADMIN_SEARCH SearchParam)
+        {
+            SP_ADMIN_EVENT_VIEW_Result eventinfo = new SP_ADMIN_EVENT_VIEW_Result();
+
+            EVENT_ADMIN_EDIT M = new EVENT_ADMIN_EDIT
+            {
+                Mode = "NEW"
+                ,
+                SearchOption = SearchParam
+                ,
+                EventInfo = eventinfo
+
+            };
+            return View("Edit", M);
+        }
+
+        public ActionResult Edit(int IDX, EVENT_ADMIN_SEARCH SearchParam)
+        {
+            EVENT_ADMIN_EDIT M = new EVENT_ADMIN_EDIT
+            {
+                Mode = "EDIT"
+                ,
+                SearchOption = SearchParam
+                ,
+                EventInfo = _eventservice.EventAdminView(IDX)
+
+            };
+            return View(M);
+        }
+
+        [HttpPost, ValidateInput(false)] 
+        [ValidateAntiForgeryToken]
+        public ActionResult EditProcess(int IDX, EVENT_SAVE_PARAM SaveParam, string MODE)
+        {
+            SP_ADMIN_EVENT_VIEW_Result Param = new SP_ADMIN_EVENT_VIEW_Result();
+            Param.EVENT_NAME = SaveParam.EVENT_NAME;
+            Param.FROM_DATE = SaveParam.FROM_DATE;
+            Param.FROM_TIME = SaveParam.FROM_TIME;
+            Param.TO_DATE = SaveParam.TO_DATE;
+            Param.TO_TIME = SaveParam.TO_TIME;
+            Param.EVENT_GBN = "A"; //A:전체 , W(웹), M (모바일)
+            Param.WEB_CONTENTS = SaveParam.WEB_CONTENTS;
+            Param.WEB_URL = SaveParam.WEB_URL;
+            Param.MOBILE_URL = SaveParam.MOBILE_URL;
+            Param.USE_YN = SaveParam.USE_YN;
+            Param.ADM_ID  = AdminUserInfo.GetAdmId();
+            Param.MOBILE_FILE = "";
+            Param.WEB_BANNER = "";
+            Param.MOBILE_BANNER = "";
+
+            string MOBILE_FILE_Name = "";
+            string WEB_BANNER_Name = "";
+            string MOBILE_BANNER_Name = "";
+
+            string MOBILE_FILE_FullName = "";
+            string WEB_BANNER_FullName = "";
+            string MOBILE_BANNER_FullName = "";
+
+            int NewIdx = 0;
+            int EventIdx = 0;
+            if (MODE == "NEW")
+            {
+                NewIdx = _eventservice.EventAdminInsert(Param);
+                EventIdx = NewIdx;
+            }
+            else
+            {
+                EventIdx = IDX;
+            }
+
+            string basepath = _eventUploadPath + Convert.ToString(EventIdx) + "/";
+
+            if (!Directory.Exists(Server.MapPath(_eventUploadPath)))
+            {
+                Directory.CreateDirectory(Server.MapPath(_eventUploadPath));
+            }
+
+            if (!Directory.Exists(Server.MapPath(basepath)))
+            {
+                Directory.CreateDirectory(Server.MapPath(basepath));
+            }
+
+            //모바일 이미지 파일 업로드
+            if (SaveParam.MOBILE_FILE != null)
+            {
+                ImageUpload imageUpload = new ImageUpload { UploadPath = basepath, addMobileImage = false };
+                ImageResult imageResult = imageUpload.RenameUploadFile(SaveParam.MOBILE_FILE);
+
+                if (imageResult.Success)
+                {
+                    MOBILE_FILE_Name = imageResult.ImageName;
+                    MOBILE_FILE_FullName = basepath + MOBILE_FILE_Name;
+                    Param.MOBILE_FILE = MOBILE_FILE_FullName;
+                }
+            }
+            else
+            {
+                if (SaveParam.MOBILE_FILE_DEL == "Y")
+                {
+                    Param.MOBILE_FILE = "";
+                }
+                else
+                {
+                    Param.MOBILE_FILE = SaveParam.OLD_MOBILE_FILE;
+                }
+            }
+
+            //모바일배너 파일 업로드
+            if (SaveParam.MOBILE_BANNER != null)
+            {
+                ImageUpload imageUpload = new ImageUpload { UploadPath = basepath, addMobileImage = false };
+                ImageResult imageResult = imageUpload.RenameUploadFile(SaveParam.MOBILE_BANNER);
+
+                if (imageResult.Success)
+                {
+                    MOBILE_BANNER_Name = imageResult.ImageName;
+                    MOBILE_BANNER_FullName = basepath + MOBILE_BANNER_Name;
+                    Param.MOBILE_BANNER = MOBILE_BANNER_FullName;
+                }
+            }
+            else
+            {
+                if (SaveParam.MOBILE_BANNER_DEL == "Y")
+                {
+                    Param.MOBILE_BANNER = "";
+                }
+                else
+                {
+                    Param.MOBILE_BANNER = SaveParam.OLD_MOBILE_BANNER;
+                }
+            }
+
+            //웹배너 업로드
+            if (SaveParam.WEB_BANNER != null)
+            {
+                ImageUpload imageUpload = new ImageUpload { UploadPath = basepath, addMobileImage = false };
+                ImageResult imageResult = imageUpload.RenameUploadFile(SaveParam.WEB_BANNER);
+
+                if (imageResult.Success)
+                {
+                    WEB_BANNER_Name = imageResult.ImageName;
+                    WEB_BANNER_FullName = basepath + WEB_BANNER_Name;
+                    Param.WEB_BANNER = WEB_BANNER_FullName;
+                }
+            }
+            else
+            {
+                if (SaveParam.WEB_BANNER_DEL == "Y")
+                {
+                    Param.WEB_BANNER = "";
+                }
+                else
+                {
+                    Param.WEB_BANNER = SaveParam.OLD_WEB_BANNER;
+                }
+            }
+
+            _eventservice.EventAdminUpdate(EventIdx, Param);
+
+            return RedirectToAction("");
+        }
+
+    }
+}
