e531c3812e90b415233ff0e42b1d0f6f558f7f50
dykimcstone <dykim@cstone.co.kr>
2015-08-07 14:36:35 +0900

    mypoint display

---
+++ AboutMe.Web.Front/Controllers/Mypage/MyPointController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AboutMe.Web.Front.Controllers.Mypage
+{
+    [RoutePrefix("MyPage/MyPoint")]
+    [Route("{action=Index}")]
+    public class MyPointController : Controller
+    {
+        // GET: MyPoint
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
