84b45882fcc05fc8b8b98a34b587fc4d61ee1835
dykimcstone <dykim@cstone.co.kr>
2015-08-07 13:17:45 +0900

    front displayer

---
+++ AboutMe.Web.Front/Controllers/ChildActionDisplayerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AboutMe.Web.Front.Controllers
+{
+    public class ChildActionDisplayerController : BaseFrontController
+    {
+        [ChildActionOnly]
+        public ActionResult BannerInProductDetail()
+        {
+            return View();
+        }
+    }
+}
