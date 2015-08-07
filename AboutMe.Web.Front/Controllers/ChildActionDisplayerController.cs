bfd27b178a0b44c38904dc13e6876042f53fa00b
dykimcstone <dykim@cstone.co.kr>
2015-08-07 13:42:05 +0900

    child displayer

--- AboutMe.Web.Front/Controllers/ChildActionDisplayerController.cs
+++ AboutMe.Web.Front/Controllers/ChildActionDisplayerController.cs
@@ -8,8 +8,32 @@ namespace AboutMe.Web.Front.Controllers
 {
     public class ChildActionDisplayerController : BaseFrontController
     {
+
+        public ChildActionDisplayerController()
+        {
+
+        }
+
         [ChildActionOnly]
         public ActionResult BannerInProductDetail()
+        {
+            return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult MainBannerInWebMain()
+        {
+            return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult MiddleBannerInWebMain()
+        {
+            return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult DisplayProductInWebMain()
         {
             return View();
         }
