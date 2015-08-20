1a7deca1bf992a4d6a72bada95a5bf40e9874e32
babocandy <babocandy@gmail.com>
2015-08-20 22:19:35 +0900

    commit

---
+++ AboutMe.Web.Mobile/Controllers/EventController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using AboutMe.Common.Helper;
+using AboutMe.Domain.Service.Promotion;
+using AboutMe.Domain.Entity.AdminPromotion;
+
+using System.Data.Entity.Core.Objects;
+using AboutMe.Common.Data;
+using Newtonsoft.Json;
+using AboutMe.Web.Mobile.Common;
+using AboutMe.Web.Mobile.Common.Filters;
+
+
+namespace AboutMe.Web.Mobile.Controllers
+{
+    public class EventController : BaseMobileController
+    {
+        // GET: Event
+
+        private IPromotionService _PromotionService;
+
+        public EventController(PromotionService _PromotionService)
+        {
+            this._PromotionService = _PromotionService;
+        }
+
+        protected override void HandleUnknownAction(string actionName)
+        {
+            this.View(actionName).ExecuteResult(this.ControllerContext);
+        }
+
+
+        public class MyMultiModelForPromotionProduct
+        {
+            public TB_PROMOTION_BY_TOTAL inst_TB_PROMOTION_BY_TOTAL { get; set; }
+            public List<SP_PROMOTION_BY_PRODUCT_TOP_1_DETAIL_SEL_Result> inst_PROMOTION_TOP_1_Result { get; set; }
+            public TB_PROMOTION_BY_PRODUCT inst_TB_PROMOTION_BY_PRODUCT { get; set; }
+            public List<SP_PROMOTION_BY_PRODUCT_PRICE_LIST_SEL_Result> inst_PROMOTION_BY_PRODUCT_PRICE_SEL_Result { get; set; }
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //타임세일 - 모바일
+        public ActionResult TimeSale()
+        {
+            ViewBag.PromotionPhotoPath = AboutMe.Common.Helper.Config.GetConfigValue("PromotionPhotoPath"); //이미지디렉토리경로
+
+            var mMyMultiModelForPromotionProduct = new MyMultiModelForPromotionProduct
+            {
+                inst_PROMOTION_TOP_1_Result = new List<SP_PROMOTION_BY_PRODUCT_TOP_1_DETAIL_SEL_Result>(),
+                inst_PROMOTION_BY_PRODUCT_PRICE_SEL_Result = new List<SP_PROMOTION_BY_PRODUCT_PRICE_LIST_SEL_Result>()
+            };
+
+
+            string CdPromotionProduct = ""; //상품별 프로모션 코드 
+
+            //타임세일 프로모션 정보중 유효한 TOP 1 가져오기 
+            mMyMultiModelForPromotionProduct.inst_PROMOTION_TOP_1_Result = _PromotionService.GetPromotionByProductTop1_Info("03").ToList();
+
+            if (mMyMultiModelForPromotionProduct.inst_PROMOTION_TOP_1_Result.Count != 0)
+            {
+                CdPromotionProduct = mMyMultiModelForPromotionProduct.inst_PROMOTION_TOP_1_Result[0].CD_PROMOTION_PRODUCT;
+                mMyMultiModelForPromotionProduct.inst_PROMOTION_BY_PRODUCT_PRICE_SEL_Result = _PromotionService.GetPromotionByProductList(CdPromotionProduct).ToList();
+
+                return View(mMyMultiModelForPromotionProduct);
+
+            }
+            else
+            {
+                return RedirectToAction("Index", "");
+            }
+
+        }
+
+
+
+        //세트상품 
+        public ActionResult SetPromotion()
+        {
+
+            ViewBag.PRODUCT_PATH = AboutMe.Common.Helper.Config.GetConfigValue("ProductPhotoPath"); //상품 이미지디렉토리경로
+            ViewBag.PROMOTION_IMG_PATH = AboutMe.Common.Helper.Config.GetConfigValue("PromotionPhotoPath"); //프로모션 이미지디렉토리경로
+
+            var mMyMultiModelForPromotionProduct = new MyMultiModelForPromotionProduct
+            {
+                inst_PROMOTION_TOP_1_Result = new List<SP_PROMOTION_BY_PRODUCT_TOP_1_DETAIL_SEL_Result>(),
+                inst_PROMOTION_BY_PRODUCT_PRICE_SEL_Result = new List<SP_PROMOTION_BY_PRODUCT_PRICE_LIST_SEL_Result>()
+            };
+
+
+            string aas = "";
+            if (TempData["jsMessage"] != null)
+            {
+                aas = TempData["jsMessage"].ToString();
+            }
+
+            string CdPromotionProduct = ""; //상품별 프로모션 코드 
+
+            //세트상품 프로모션 정보중 유효한 TOP 1 가져오기 
+            mMyMultiModelForPromotionProduct.inst_PROMOTION_TOP_1_Result = _PromotionService.GetPromotionByProductTop1_Info("01").ToList();
+
+            if (mMyMultiModelForPromotionProduct.inst_PROMOTION_TOP_1_Result.Count != 0)
+            {
+                CdPromotionProduct = mMyMultiModelForPromotionProduct.inst_PROMOTION_TOP_1_Result[0].CD_PROMOTION_PRODUCT;
+                mMyMultiModelForPromotionProduct.inst_PROMOTION_BY_PRODUCT_PRICE_SEL_Result = _PromotionService.GetPromotionByProductList(CdPromotionProduct).ToList();
+
+
+                return View(mMyMultiModelForPromotionProduct);
+            }
+            else
+            {
+                return RedirectToAction("Index", "");
+            }
+
+
+            //return View();
+        }
+
+    }
+}
