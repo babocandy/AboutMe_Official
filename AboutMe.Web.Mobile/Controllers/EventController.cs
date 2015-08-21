d0fba8bef32c2f4446b2c999e1ce3b87093053f2
bckang <bckang@gxensoft.com>
2015-08-21 10:28:48 +0900

    모바일 이벤트, ㄱㅣ획전

--- AboutMe.Web.Mobile/Controllers/EventController.cs
+++ AboutMe.Web.Mobile/Controllers/EventController.cs
@@ -7,6 +7,10 @@ using System.Web.Mvc;
 using AboutMe.Common.Helper;
 using AboutMe.Domain.Service.Promotion;
 using AboutMe.Domain.Entity.AdminPromotion;
+using AboutMe.Domain.Service.Event;
+using AboutMe.Domain.Entity.Event;
+using AboutMe.Domain.Service.Exhibit;
+using AboutMe.Domain.Entity.Exhibit;
 
 using System.Data.Entity.Core.Objects;
 using AboutMe.Common.Data;
@@ -22,10 +26,14 @@ namespace AboutMe.Web.Mobile.Controllers
         // GET: Event
 
         private IPromotionService _PromotionService;
+        private IEventService _eventservice;
+        private IExhibitService _exhibitservice;
 
-        public EventController(PromotionService _PromotionService)
+        public EventController(PromotionService _PromotionService, IEventService _eventservice, IExhibitService _exhibitservice)
         {
             this._PromotionService = _PromotionService;
+            this._eventservice = _eventservice;
+            this._exhibitservice = _exhibitservice;
         }
 
         protected override void HandleUnknownAction(string actionName)
@@ -42,10 +50,66 @@ namespace AboutMe.Web.Mobile.Controllers
             public List<SP_PROMOTION_BY_PRODUCT_PRICE_LIST_SEL_Result> inst_PROMOTION_BY_PRODUCT_PRICE_SEL_Result { get; set; }
         }
 
+
+        #region 이벤트메인/ 이벤트 / 기획전 (gxen) ================================================================
+
+            // GET: Main
         public ActionResult Index()
         {
-            return View();
+            List<SP_PROMOTION_BY_PRODUCT_TOP_1_DETAIL_SEL_Result> timesaleList = new List<SP_PROMOTION_BY_PRODUCT_TOP_1_DETAIL_SEL_Result>();
+
+            //타임세일 프로모션 정보중 유효한 TOP 1 가져오기 
+            timesaleList = _PromotionService.GetPromotionByProductTop1_Info("03").ToList();
+            EVENT_MAIN_INDEX M = new EVENT_MAIN_INDEX
+            {
+                TimeSaleCnt = timesaleList.Count(),
+                MainInfo = _eventservice.EventMainView(),
+                IngListInfo = _eventservice.EventMainIngList(),
+                EndListInfo = _eventservice.EventMainEndList()
+            };
+            return View(M);
         }
+
+        public ActionResult EventView(int IDX)
+        {
+            EVENT_VIEW M = new EVENT_VIEW
+            {
+                EventInfo = _eventservice.EventView(IDX),
+                IngListInfo = _eventservice.EventMainIngList()
+            };
+            return View(M);
+        }
+
+        public ActionResult ExhibitView(int IDX)
+        {
+            List<SP_ADMIN_EXHIBIT_TAB_SEL_Result> Tablist = _exhibitservice.ExhibitAdminTabList(IDX);
+            List<EXHIBIT_TAB_PRODUCT> TabProduct = new List<EXHIBIT_TAB_PRODUCT>();
+            foreach (SP_ADMIN_EXHIBIT_TAB_SEL_Result item in Tablist)
+            {
+                EXHIBIT_TAB_PRODUCT tp = new EXHIBIT_TAB_PRODUCT
+                {
+                    Tabinfo = item,
+                    ProductList = _exhibitservice.ExhibitTabProductList(item.IDX)
+                };
+                TabProduct.Add(tp);
+            }
+
+            EXHIBIT_VIEW M = new EXHIBIT_VIEW
+            {
+                ExhibitInfo = _exhibitservice.ExhibitView(IDX),
+                TabProductList = TabProduct,
+                IngList = _eventservice.EventMainIngList()
+            };
+            return View(M);
+        }
+
+        //상품상세 : 우측하단의 이벤트/기획전 목록
+        public ActionResult EventInProductDetail(string p_code)
+        {
+            List<SP_EXHIBIT_PCODE_LINK_ALL_Result> M = _exhibitservice.ExhibitProductLinkAll(p_code);
+            return PartialView(M);
+        }
+        #endregion
 
         //타임세일 - 모바일
         public ActionResult TimeSale()
