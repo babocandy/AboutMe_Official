b62c8373eb72448c4473fd30d671df1a0bb186f8
bright8888 <bright8888@nate.com>
2015-08-31 18:59:21 +0900

    jsh MAILTEST

--- AboutMe.Web.Admin/Controllers/HomeController.cs
+++ AboutMe.Web.Admin/Controllers/HomeController.cs
@@ -5,10 +5,13 @@ using System.Web;
 using System.Web.Mvc;
 
 using AboutMe.Common.Helper;
+using AboutMe.Web.Admin.Common;
+
+using AboutMe.Web.Admin.Common.Filters;
 
 namespace AboutMe.Web.Admin.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseAdminController
     {
       
         public ActionResult Index()
@@ -53,6 +56,44 @@ namespace AboutMe.Web.Admin.Controllers
             return View();
         }
         #endregion
+
+
+        [CustomAuthorize] //어드민로그인 필요 //[CustomAuthorize(Roles = "S")] //수퍼어드민만 가능 
+        public ActionResult MailTest()
+        {
+            return View();
+        }
+
+        [CustomAuthorize] //어드민로그인 필요 //[CustomAuthorize(Roles = "S")] //수퍼어드민만 가능 
+        public ActionResult SendMailGo(string receiver = "", string subject = "[AboutMe]No Subject", string body = "")
+        {
+
+            //메일 발송을 위한 발송정보 준비 ----------------------------------------------------
+            string MAIL_SENDER_EMAIL = Config.GetConfigValue("MAIL_SENDER_EMAIL"); //noreply@cstone.co.kr
+            string MAIL_SENDER_PW = Config.GetConfigValue("MAIL_SENDER_PW"); //cstonedev12
+            string MAIL_SENDER_SMTP_SERVER = Config.GetConfigValue("MAIL_SENDER_SMTP_SERVER"); //smtp.gmail.com
+            string MAIL_SENDER_SMTP_PORT = Config.GetConfigValue("MAIL_SENDER_SMTP_PORT"); //587
+            string MAIL_SENDER_SMTP_TIMEOUT = Config.GetConfigValue("MAIL_SENDER_SMTP_TIMEOUT"); //20000
+
+            //메일 발송
+            MailSender mObj = new MailSender();
+            mObj.MailSendAction(MAIL_SENDER_EMAIL, MAIL_SENDER_PW, MAIL_SENDER_SMTP_SERVER, MAIL_SENDER_SMTP_PORT, MAIL_SENDER_SMTP_TIMEOUT, receiver, subject, body);
+
+
+            ViewBag.mail_err_no = mObj.err_no;
+            ViewBag.mail_err_msg = mObj.err_msg;
+
+            ViewBag.mail_sender = mObj.sender;
+            ViewBag.mail_receiver = mObj.receiver;
+            ViewBag.mail_subject = mObj.subject;
+            ViewBag.mail_body = mObj.body;
+
+  
+
+
+            return View();
+        }
+
 
     }
 }
