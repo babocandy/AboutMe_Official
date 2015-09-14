0ca3764e20527e3788e415481c6b9679ba036e4b
bright8888 <bright8888@nate.com>
2015-09-14 10:54:28 +0900

    jsh-Admin Member Default:Retire:N

--- AboutMe.Web.Admin/App_Start/RouteConfig.cs
+++ AboutMe.Web.Admin/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace AboutMe.Web.Admin
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "AdminFrontMember", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Order", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
